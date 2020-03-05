497bf59b188c5e6435d40ba8c816e77c8d9245c1
ktts16 <ktts16@hotmail.com>
2020-03-05 10:58:55 +0700

    Add IPython display functions

---
+++ utils_ipython_display.py
@@ -0,0 +1,15 @@
+from IPython.display import Markdown, display
+
+def printmd(string, color=None):
+    colorstr = "<span style='color:{}'>{}</span>".format(color, string)
+    display(Markdown(colorstr))
+
+
+def md_bold(string):
+    return "**" + string + "**"
+
+
+def show_hyperlink(url, text=None):
+    if text is None:
+        text = url
+    display(HTML("<a href='" + url + "' target='_blank'>" + text + "</a>"))
